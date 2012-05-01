a00db7af2882a0cd303614d4c66da53121a59874
Michael LaCroix <michael@lacroixdesign.net>
2012-05-01 00:46:32 -0500

    switched app-specific names in deploy.rb to generic placeholders

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,24 +3,24 @@ require "bundler/capistrano"
 load "config/recipes/base"
 load "config/recipes/nginx"
 load "config/recipes/unicorn"
-load "config/recipes/mongodb"
-# load "config/recipes/postgresql"
+# load "config/recipes/mongodb"
+load "config/recipes/postgresql"
 load "config/recipes/nodejs"
 load "config/recipes/rbenv"
 load "config/recipes/check"
 
-server "50.56.191.114", :web, :app, :db, primary: true
+server "ip.address.of.server", :web, :app, :db, primary: true
 
 set :user, "deploy"
-set :application, "flyby"
+set :application, "application_name"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@lacroixdesign.net:flyby-rackspace.git"
-set :remote_repository, "server" # default is "origin"
-set :branch, "rackspace" # default is "master"
+set :repository, "git@lacroixdesign.net:application_name.git"
+set :remote_repository, "origin" # default is "origin"
+set :branch, "master" # default is "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
