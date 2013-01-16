c949dbf6448b9a8e224c3cc658c3e8ee9506445f
Michael LaCroix <michael@lacroixdesign.net>
2013-01-15 20:25:59 -0600

    final adjustments before switch to lightweight template

--- resources/app/helpers/application_helper.rb
+++ resources/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-	# Page Titles - Set individual page title elements
+  # Page Titles - Set individual page title elements
   # Accepts a String or Array.
   # Sets yield(:title) to a String for use in <title>.
   # 
@@ -20,68 +20,82 @@ module ApplicationHelper
     end
     content_for(:title) { title }
   end
-  
-	
-	# 
-	# Yes -or- No
-	# 
-	def yes_no(bool)
-		bool ? 'Yes' : 'No'
-	end
 
-	
-	# 
-	# Enabled -or- Disabled
-	# 
-	def enabled_disabled(bool)
-		bool ? 'Enabled' : 'Disabled'
-	end
-	
-	
-	# 
-	# Substitute N/A for blank data
-	# 
-	def check(data)
-		data.blank? ? 'N/A' : data
-	end
-	
-	
-	
-	# 
-	# Date: Jan 1, 2012
-	# 
-	def date_short(date)
-		date.strftime("%b %e, %Y") if !date.blank?
-	end
-	
-	
-	# 
-	# Date: 1/1/2012
-	# 
-	def date_compact(date)
-		date.strftime("%-m/%-d/%Y") if !date.blank?
-	end
-	
-	
-	# 
-	# Substitute empty string for blank data
-	# 
-	def exists?(data)
-		data.blank? ? '' : data
-	end
-	
-	
-	
-	
-	
-	
-	# 
-	# Alternate object debug
-	# 
-	def mdebug(object)
-		d = object.attributes.inspect.split(',')
-		d.map! {|i| i.to_s + '<br>'}
-		d.join.delete("{").delete("}")
-	end
-	
+  # Flash Messages
+  # Iterate through each standard flash type
+  # Detect whether the alert is a Hash -or- String
+  # 
+  #   --String--
+  #   flash.notice = "Example title"
+  # 
+  #   --Hash--
+  #   flash.notice = { title: "Example title", message: "Expanded detailed message." }
+  # 
+  def flash_helper
+    flash_types = { message: 'bullhorn', 
+        notice: 'ok', 
+        warning: 'warning-sign', 
+        alert: 'exclamation-sign' }
+    flash_messages = []
+    flash_types.each do |type, icon|
+      # --- Check if this :type exists
+      if flash[type]
+        if flash[type].is_a?(Hash)
+          # --- Format => { title: "Alert Title", message: "Longer alert message detail." }
+          title   = flash[type].fetch(:title, nil)
+          message = flash[type].fetch(:message, nil)
+        elsif flash[type].is_a?(String)
+          title   = flash[type]
+        end
+        # --- Skip if title was not set
+        if title
+          alert =  "<li class=\"#{type}\"><a class=\"close\" href=\"#\">x</a>"
+          alert += "<div class=\"inner\"><div class=\"content\">"
+          alert += "<div class=\"title\"><p><i class=\"icon-#{icon}\"></i> #{title} "
+          # alert += "<i class=\"icon-caret-down\"></i> " if message
+          alert += "<small>@ #{Time.now.strftime("%l:%M%P : %Ss")}</small></p></div>"
+          alert += "<div class=\"message\"><p>#{message}</p></div>" if message
+          alert += "</div></div></li>"
+          flash_messages << alert
+        end
+      end
+    end
+    flash_messages.join
+  end
+  
+  # Yes -or- No
+  # 
+  def yes_no(bool)
+    bool ? 'Yes' : 'No'
+  end
+  
+  # Enabled -or- Disabled
+  # 
+  def enabled_disabled(bool)
+    bool ? 'Enabled' : 'Disabled'
+  end  
+  
+  # Substitute N/A for blank data
+  # 
+  def check(data)
+    data.blank? ? 'N/A' : data
+  end  
+  
+  # Date: Jan 1, 2012
+  # 
+  def date_short(date)
+    date.strftime("%b %e, %Y") unless date.blank?
+  end  
+  
+  # Date: 1/1/2012
+  # 
+  def date_compact(date)
+    date.strftime("%-m/%-d/%Y") unless date.blank?
+  end  
+  
+  # Substitute empty string for blank data
+  # 
+  def exists?(data)
+    data.blank? ? '' : data
+  end 
 end
